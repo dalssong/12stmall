e3db8c9331299da5b2bb1a4b1b422b3bc94dd3c1
dalssong <126850101+dalssong@users.noreply.github.com>
2023-05-09 09:36:21 +0900

    commit

---
+++ delivery/src/main/java/stmall/domain/OrderPlaced.java
@@ -0,0 +1,17 @@
+package stmall.domain;
+
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private Long productId;
+    private Long userId;
+    private String productName;
+    private Integer qty;
+}
---
+++ order/src/main/java/stmall/domain/OrderPlaced.java
@@ -0,0 +1,25 @@
+package stmall.domain;
+
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private Long productId;
+    private Long userId;
+    private String productName;
+    private Integer qty;
+
+    public OrderPlaced(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderPlaced() {
+        super();
+    }
+}
