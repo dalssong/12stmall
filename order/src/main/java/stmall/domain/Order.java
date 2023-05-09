4a58c3759edafc418f10d4080b5ea392ab4a0893
dalssong <126850101+dalssong@users.noreply.github.com>
2023-05-09 09:53:38 +0900

    commit

--- order/src/main/java/stmall/domain/Order.java
+++ order/src/main/java/stmall/domain/Order.java
@@ -33,8 +33,8 @@ public class Order {
         orderPlaced.publishAfterCommit();
     }
 
-    @PrePersist
-    public void onPrePersist() {
+    @PreRemove
+    public void onPreRemove() {
         OrderCanceled orderCanceled = new OrderCanceled(this);
         orderCanceled.publishAfterCommit();
     }
